e0a7c14706a1fe855238d16b6e978a698448d9a9
Gevik Karimian <gevik@live.com>
2016-06-26 01:53:57 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+// 	name: 'Gevik',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Gevik',
+// 	// 	age: 35
+// 	// };
+// 	obj.age = 35;
+// }
+
+// updatePerson(person);
+
+// console.log(person);
+
+var grades = [10, 20];
+
+function addGrade(obj, a, b) {
+	obj.push(a, b);
+	debugger;
+}
+
+addGrade(grades, 30, 40);
+console.log(grades);
