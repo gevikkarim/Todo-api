8f84e11ac254fae59d6cbb9aaa352da9a219aa1e
Gevik Karimian <gevik@live.com>
2016-07-05 14:00:05 -0700

    Add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 var bcrypt = require('bcryptjs');
 var _ = require('underscore');
 
-module.exports = function (sequelize, DataTypes) {
-	return sequelize.define('user', {
+module.exports = function(sequelize, DataTypes) {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -23,10 +23,10 @@ module.exports = function (sequelize, DataTypes) {
 			validate: {
 				len: [7, 100]
 			},
-			set: function (value) {
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
-			
+
 				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
@@ -35,17 +35,42 @@ module.exports = function (sequelize, DataTypes) {
 
 	}, {
 		hooks: {
-			beforeValidate: function (user, option) {
+			beforeValidate: function(user, option) {
 				if (typeof user.email === 'string') {
-					user.email =user.email.toLowerCase();
+					user.email = user.email.toLowerCase();
 				}
 			}
 		},
-		instanceMethods : {
-			toPublicJSON: function () {
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+
+				});
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
+
+	return user;
 };
